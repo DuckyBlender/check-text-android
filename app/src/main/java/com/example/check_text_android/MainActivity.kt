271a4c8426af2659c262dda72a0b0ebf2a150986
DuckyBlender <alan@alanklas.com>
2023-02-07 23:48:00 +0100

    almost done

--- app/src/main/java/com/example/check_text_android/MainActivity.kt
+++ app/src/main/java/com/example/check_text_android/MainActivity.kt
@@ -2,42 +2,70 @@ package com.example.check_text_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckedTextView
 import android.widget.EditText
 import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Initialize the inputs and views
         val passInput = findViewById<EditText>(R.id.passInput)
-        val passCheck = findViewById<EditText>(R.id.passConfirm)
+        val passConfirm = findViewById<EditText>(R.id.passConfirm)
 
-        val zgodneView = findViewById<TextView>(R.id.zgodneView)
-        val duzaView = findViewById<TextView>(R.id.duzaView)
-        val malaView = findViewById<TextView>(R.id.malaView)
-        val cyfraView = findViewById<TextView>(R.id.cyfraView)
-        val znakView = findViewById<TextView>(R.id.znakView)
+        val zgodne = findViewById<CheckedTextView>(R.id.zgodne)
+        val duza = findViewById<CheckedTextView>(R.id.duza)
+        val mala = findViewById<CheckedTextView>(R.id.mala)
+        val cyfra = findViewById<CheckedTextView>(R.id.cyfra)
+        val znak = findViewById<CheckedTextView>(R.id.znak)
 
-        fun checkPass() {
-            val pass = passInput.text.toString()
-            val passConfirm = passCheck.text.toString()
-            val zgodne = pass == passConfirm
-            val duza = pass.any { it.isUpperCase() }
-            val mala = pass.any { it.isLowerCase() }
-            val cyfra = pass.any { it.isDigit() }
-            val znak = pass.any { it.isLetterOrDigit() }
+        val button = findViewById<Button>(R.id.button)
 
-            zgodneView.text = "Hasła zgodne: $zgodne"
-            duzaView.text = "Hasło zawiera dużą literę: $duza"
-            malaView.text = "Hasło zawiera małą literę: $mala"
-            cyfraView.text = "Hasło zawiera cyfrę: $cyfra"
-            znakView.text = "Hasło zawiera znak specjalny: $znak"
+        fun checkPass(): Boolean {
+            val passText = passInput.text.toString()
+            val confirmText = passConfirm.text.toString()
+            val zgodneBool = passText == confirmText
+            val duzaBool = passText.any { it.isUpperCase() }
+            val malaBool = passText.any { it.isLowerCase() }
+            val cyfraBool = passText.any { it.isDigit() }
+            val znakBool = passText.any { it.isLetterOrDigit() }
+
+            // Set the state of the CheckedTextViews
+            zgodne.isChecked = zgodneBool
+            duza.isChecked = duzaBool
+            mala.isChecked = malaBool
+            cyfra.isChecked = cyfraBool
+            znak.isChecked = znakBool
+
+            // Enable the button if all the conditions are met
+            val wynik = zgodneBool && duzaBool && malaBool && cyfraBool && znakBool
+
+
+            button.isEnabled = wynik
+            return wynik
         }
 
-        // Listen for changes in the password input
-        passInput.addTextChangedListener {
-            checkPass()
-        }
+        // Listen for changes in the password input or the password confirmation input
+        passInput.addTextChangedListener { checkPass() }
+        passConfirm.addTextChangedListener { checkPass() }
 
+        // Listen for clicks on the button
+        button.setOnClickListener {
+            if (checkPass()) {
+                // Pop up a message saying that the password is correct using snackbar
+                var snackbar = Snackbar.make(findViewById(R.id.button), "Hasło poprawne", Snackbar.LENGTH_LONG)
+                snackbar.show()
+            }
+            else {
+                // Pop up a message saying that the password is incorrect
+                var snackbar = Snackbar.make(findViewById(R.id.button), "Hasło niepoprawne", Snackbar.LENGTH_LONG)
+                snackbar.show()
+            }
+        }
+    }
 }
